5babe820d35dacefe9af973aef3c143235a79ebb
Dmitriy <work@prr133f.ru>
2026-02-20 21:44:22 +0300

    fix: Fix golangci plugin
    Rewrite plugin to API v2

--- plugin/main.go
+++ plugin/main.go
@@ -1,10 +1,25 @@
 package plugin
 
 import (
+	"github.com/golangci/plugin-module-register/register"
 	analyzer "github.com/prr133f/go-log-linter/analyzers/log-linter"
 	"golang.org/x/tools/go/analysis"
 )
 
-func New(settings any) ([]*analysis.Analyzer, error) {
+func init() {
+	register.Plugin("loglinter", New)
+}
+
+func New(settings any) (register.LinterPlugin, error) {
+	return LogLinterPlugin{}, nil
+}
+
+type LogLinterPlugin struct{}
+
+func (p LogLinterPlugin) BuildAnalyzers() ([]*analysis.Analyzer, error) {
 	return []*analysis.Analyzer{analyzer.New()}, nil
+}
+
+func (p LogLinterPlugin) GetLoadMode() string {
+	return register.LoadModeTypesInfo
 }
